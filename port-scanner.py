f7a518ca5684118cb8a833f316ed8ce62e9f60e7
root <root@Loki.localdomain>
2025-12-30 22:36:28 -0600

    updates the port-scanner.py to include more granularity for exceptions and supports args as cli util now

--- port-scanner.py
+++ port-scanner.py
@@ -1,26 +1,39 @@
 import socket
 import concurrent.futures
 import time
+import sys
+import argparse
 
 def port_scan(host, port):
-    
     s = socket.socket()
-
     try: 
         s.settimeout(2)
         s.connect((host,port))
         return True
-    except:
+    except(OSError, socket.timeout):
         return False
     finally:
         s.close()
 
 def main():
+    parser = argparse.ArgumentParser(description="Usage: Scan PortStart to PortEnd on Host")
+
+    parser.add_argument("host", type=str, help="The Host you wish to scan")
+    parser.add_argument("ps", type=int, help="port to start the scan from before incrementing to the next port")
+    parser.add_argument("pe", type=int, help="port to end the scan at")
+
+    args = parser.parse_args()
+
     futures_to_port={} # Used for tracking the port-to-future (thread) so we can see it on the other "end" of the concurrency.
+    
     start_time = time.time()
-    targetHost="scanme.nmap.org"
-    with concurrent.futures.ThreadPoolExecutor() as executor:   
-        futures_to_port = {executor.submit(port_scan,targetHost,port): port for port in range(20,101)}
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:   
+        futures_to_port = {
+            executor.submit(port_scan,args.host,port): port 
+            for port in range(args.ps, args.pe + 1)
+        }
+
         for future in concurrent.futures.as_completed(futures_to_port):
             port = futures_to_port[future]
             if future.result():
